a34f1026412882272bb8dbf750f73160ea2522e0
Adrian Mihai <adrian@carve.ro>
2011-03-17 21:33:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roadie::Application.config.secret_token = '78bbfa79e8a81d6f89ac885150c7ef1fc166d538697e355ed74b2c02f24e44f10449b78b7737a93653deb81fdd1c46bde45dd24581df36587dbdb0e9a4a60dd0'
