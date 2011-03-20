a8ee95ab18884dfd53033a817ef67ecfb1c6cd96
Adrian Mihai <adrian@carve.ro>
2011-03-20 23:51:49 +0200

    fixed a problem in migrations

--- db/migrate/20110320172214_remove_section_length_from_points.rb
+++ db/migrate/20110320172214_remove_section_length_from_points.rb
@@ -1,7 +1,7 @@
 class RemoveSectionLengthFromPoints < ActiveRecord::Migration
   def self.up
-    remove_column :points, :section_length, :decimal
-    remove_column :points, :section, :string
+    remove_column :points, :section_length
+    remove_column :points, :section
   end
 
   def self.down
