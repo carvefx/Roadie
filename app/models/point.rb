2d970c7790a438e73d14297da30a97167d4bc67a
Adrian Mihai <adrian@carve.ro>
2011-03-22 17:16:32 +0200

    Removed the check for the presence_of height, d1 and d2 because they might not be needed if the elevation is known

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,4 +1,4 @@
 class Point < ActiveRecord::Base
   belongs_to :section
-  validates_presence_of :name, :distance, :d1, :d2, :section, :height
+  validates_presence_of :name, :distance, :section
 end
